2b654cc14cad46b2717fb859aacb08be2baf2e02
nick veniba <cryptomilk2021@gmail.com>
2022-08-23 10:51:39 +1000

    complete Procfile

--- main.py
+++ main.py
@@ -218,6 +218,9 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
+#
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=5000)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
